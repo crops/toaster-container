be6be6eb21b517a394353eeb2f18dba5a117c50a
Randy Witt <randy.e.witt@linux.intel.com>
2016-07-26 14:40:54 -0700

    tests: Add a smoketest for the container
    Using runtests.sh the toaster container will now be used to do a build
    of quilt-native. This is to check that the basically functionality and
    goal of the container works.
    
    Signed-off-by: Randy Witt <randy.e.witt@linux.intel.com>

---
+++ tests/runtests.sh
@@ -0,0 +1,177 @@
+#!/bin/bash
+
+# runtests.sh
+#
+# Copyright (C) 2016 Intel Corporation
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+# Variables you might care about:
+# SELENIUM_VERSION
+#    This is the version of selenium that will be used for the selenium
+#    container and the selenium modules installed in the virtualenv. The
+#    default is 2.53.0.
+#
+# IMAGE
+#    This is the image to be used for toaster. It should be in the same format
+#    as passed to "docker run". By default, it is set to
+#    "crops/toaster:latest".
+#
+# POKYBRANCH
+#    If set, this is the branch of poky that will be used by toaster when doing
+#    a build. i.e. "jethro", "krogoth", "master"
+#    By default it is "master".
+#
+# VNCPORT
+#    When set, the selenium container will be ran with a vncserver listening
+#    on this port. The password for the selenium vnc server is "secret". Note,
+#    you cannot of course connect to the vnc server until the selenium
+#    container has finished starting.
+#
+# SHOW_LOGS_ON_FAILURE
+#    When set, if the tests fail, the contents of toaster.log and selenium.log
+#    will be output to the terminal. This is useful for when running on travis.
+
+set -e
+
+function stop_containers () {
+    docker kill $toastername $seleniumname >& /dev/null
+}
+
+trap fail SIGINT SIGTERM ERR
+function fail () {
+    kill $(jobs -p)
+    stop_containers
+
+    echo "FAILURE: See logs in $tempdir"
+    if [ "" != "$SHOW_LOGS_ON_FAILURE" ]; then
+         printf "******************toaster.log*******************"
+	 cat $toasterlog
+         printf "******************toaster.log*******************\n\n"
+
+	 printf "******************selenium.log******************"
+	 cat $seleniumlog
+	 printf "******************selenium.log******************\n\n"
+    fi
+
+    exit 1
+}
+
+function start_toaster() {
+    mkdir $tempdir/toasterbuild
+
+    touch $toasterlog
+    sentinel="Successful start."
+
+    # This convoluted command will output the logfile until the sentinel is
+    # found.
+    bash -c "tail -n +0 -f $toasterlog | \
+             { sed '/$sentinel/ q' && kill \$\$;}" &
+
+    printf "\n\nStarting toaster...\n"
+    docker run -t --rm=true --name=$toastername \
+               -v $tempdir/toasterbuild:/workdir \
+               $image >> $toasterlog 2>&1 &
+    toasterpid=$!
+
+    while ! grep "$sentinel" $toasterlog >& /dev/null; do
+        # Check if the job exited
+        if [ "$(ps -p $toasterpid -o comm=)" != "docker" ] ; then
+    	    echo "ERROR: The toaster job couldn't be found."
+            fail
+        fi
+        sleep 1
+    done
+}
+
+function start_selenium() {
+    touch $seleniumlog
+    sentinel="Selenium Server is up and running"
+    # This convoluted command will output the logfile until the sentinel is
+    # found.
+    bash -c "tail -n +0 -f $seleniumlog | \
+             { sed '/$sentinel/ q' && kill \$\$;}" &
+
+    printf "\n\nStarting selenium...\n"
+    if [ "$VNCPORT" != "" ]; then
+        docker run --rm=true -p 127.0.0.1:$VNCPORT:5900 \
+                   -p 127.0.0.1:4444:4444 --name=$seleniumname \
+                   --link=$toastername \
+                   selenium/standalone-firefox-debug:$selenium_version \
+                   >> $seleniumlog 2>&1 &
+    else
+        docker run --rm=true -p 127.0.0.1:4444:4444 --name=$seleniumname \
+                   --link=$toastername \
+                   selenium/standalone-firefox:$selenium_version \
+                   >> $seleniumlog 2>&1 &
+    fi
+    seleniumpid=$!
+
+    while ! grep "$sentinel" $seleniumlog >& /dev/null; do
+        # Check if the job exited
+        if [ "$(ps -p $seleniumpid -o comm=)" != "docker" ] ; then
+    	    echo "ERROR: The selenium job couldn't be found."
+            fail
+        fi
+        sleep 1
+    done
+}
+
+tempdir=$(mktemp -d --suffix toastertest --tmpdir)
+echo "Logs in $tempdir"
+
+id=$(uuidgen)
+toastername=toasterserver-$id
+seleniumname=seleniumserver-$id
+
+toasterlog=$tempdir/toaster.log
+seleniumlog=$tempdir/selenium.log
+
+if [ "" != "$IMAGE" ]; then
+    image="$IMAGE"
+else
+    image="crops/toaster:latest"
+fi
+
+if [ "" != "$SELENIUM_VERSION" ]; then
+    selenium_version="$SELENIUM_VERSION"
+else
+    selenium_version=2.53.0
+fi
+
+if [ "" != "$POKYBRANCH" ]; then
+    pokybranch_arg="--pokybranch=$POKYBRANCH"
+fi
+
+
+# Create a virtualenv that contains the modules needed by smoketests.py
+virtualenv $tempdir/selenium
+. $tempdir/selenium/bin/activate
+pip install selenium==$selenium_version
+
+
+# Run the containers
+start_toaster
+start_selenium
+
+
+printf "\n\nRunning tests...\n"
+./smoketests.py --toaster_url="$toastername:8000" \
+                $pokybranch_arg
+./checkartifacts.sh $tempdir/toasterbuild/build-toaster-2
+echo "TESTS PASSED!"
+
+
+stop_containers
+rm $tempdir -rf
---
+++ tests/smoketests.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python
+# smoketests.py
+#
+# Copyright (C) 2016 Intel Corporation
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+import argparse
+import os
+import sys
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--server_url", default="http://localhost:4444/wd/hub",
+                    help="url of the selenium server")
+parser.add_argument("--toaster_url", default="http://localhost:8000",
+                    help="url of the toaster server")
+parser.add_argument("--pokybranch", default="master",
+                    help="poky branch to use for build")
+parser.add_argument("--target", default="quilt-native",
+                    help="target that toaster should build")
+
+args = parser.parse_args()
+driver = webdriver.Remote(args.server_url,
+                          webdriver.DesiredCapabilities.FIREFOX.copy())
+
+driver.get(args.toaster_url)
+
+failed=False
+
+try:
+    # Click the new project button
+    element = driver.find_element_by_id("new-project-button")
+    element.click()
+
+    # Wait for the new project page to actually appear
+    ec = EC.presence_of_element_located((By.ID, "new-project-name"))
+    WebDriverWait(driver, 30).until(ec)
+
+    # Type the project name
+    element = driver.find_element_by_id("new-project-name")
+    element.send_keys("foo")
+
+    # Pick the poky branch that matches the branch specified
+    element = driver.find_element_by_id("projectversion")
+    options = element.find_elements_by_tag_name("option")
+    optionfound = False
+    for option in options:
+        if args.pokybranch in option.text.lower():
+            option.click()
+            optionfound = True
+
+    # Bail out if we couldn't find the branch
+    if not optionfound:
+        textoptions = '", "'.join([ x.text.lower() for x in options ])
+        msg = 'Error: Branch {} not in options: "{}"'.format(args.pokybranch,
+                                                             textoptions)
+        raise Exception(msg)
+
+    # Actually click the button to create the project
+    element = driver.find_element_by_id("create-project-button")
+    element.click()
+
+    # Type in the target to build
+    element = driver.find_element_by_id("build-input")
+    element.send_keys(args.target)
+
+    # Click the button to start the build
+    element = driver.find_element_by_id("build-button")
+    element.click()
+
+    # Wait for either a success or a fail
+    selector = ("div.alert.build-result.alert-success,"
+                "div.alert.build-result.alert-error")
+    ec = EC.presence_of_element_located((By.CSS_SELECTOR, selector))
+    element = WebDriverWait(driver, 120).until(ec)
+
+    # If the build failed bail out
+    if "alert-error" in element.get_attribute("class"):
+        raise Exception("ERROR: Build of {} failed.".format(args.target))
+
+except Exception as e:
+    failed=True
+    path = os.path.abspath('screenshot.png')
+    print("{}\nAttempting to save screenshot to {}".format(e, path))
+    driver.save_screenshot(path)
+
+finally:
+    driver.quit()
+
+if failed:
+    sys.exit(1)
