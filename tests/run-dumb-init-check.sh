8cda07f52c611dd114c3bdcaf374a0b2799bfbbe
brian avery <brian.avery@intel.com>
2017-06-19 11:45:15 -0700

    tests: Limit dumb-init test string match
    The current dumb-init test fails if you are running the test in --local
    mode.  This patch limits the substring match to only insure that
    dumb-init is running and that it is pid 1.
    
    Signed-off-by: brian avery <brian.avery@intel.com>
    Signed-off-by: Randy Witt <randy.e.witt@linux.intel.com>

--- tests/run-dumb-init-check.sh
+++ tests/run-dumb-init-check.sh
@@ -23,15 +23,15 @@ container="$1"
 
 username="usersetup"
 username_width=${#username}
-expected="1 $username /usr/bin/dumb-init -- /usr/bin/toaster-entry.py"
+required="^1 $username /usr/bin/dumb-init"
 
 # Use bash -c, because otherwise you must specify an absolute path to ps,
 # because $PATH would not be set.
 actual=`docker exec $container bash -c "ps -w -w h -C dumb-init -o pid:1,user:$username_width,args"`
 
-if [ "$expected" != "$actual" ]; then
-    printf "expected dumb-init not found\n"
-    printf "expected:\n%s\n" "$expected"
+if [ "$actual" =~ "$required" ]; then
+    printf "required dumb-init not found\n"
+    printf "required:\n%s\n" "$required"
     printf "actual:\n%s\n" "$actual"
     printf "all:\n"
     docker exec $container bash -c "ps -w -w -A -o pid,user:$username_width,args"
