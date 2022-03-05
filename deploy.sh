c4ca7212151f2685b9059d1f68c09d7c49615ed2
Tim Orling <tim.orling@konsulko.com>
2022-03-04 20:41:01 -0800

    deploy.sh: SPDX license header
    Signed-off-by: Tim Orling <tim.orling@konsulko.com>

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,10 @@
 #!/bin/bash
 # Copyright (C) 2016 Intel Corporation
+# Copyright (C) 2022 Konsulko Group
 #
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License version 2 as
-# published by the Free Software Foundation.
+# SPDX-License-Identifier: GPL-2.0-only
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License along
-# with this program; if not, write to the Free Software Foundation, Inc.,
-# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-#
-# This script is meant to be consumed by travis. It's very simple but running
-# a loop in travis.yml isn't a great thing.
+# This script is meant to be consumed by GitHub Actions.
 set -e
 
 # Don't deploy on pull requests because it could just be junk code that won't
