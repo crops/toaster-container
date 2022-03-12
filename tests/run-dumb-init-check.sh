c08a6e6359e936fb75bfff8597f605ccb1c4c5b4
Tim Orling <tim.orling@konsulko.com>
2022-03-11 20:27:55 -0800

    run-dumb-init-check.sh: improve regex check
    Use double [] to silence binary operator warning
    
    Signed-off-by: Tim Orling <tim.orling@konsulko.com>

--- tests/run-dumb-init-check.sh
+++ tests/run-dumb-init-check.sh
@@ -23,13 +23,13 @@ container="$1"
 
 username="usersetup"
 username_width=${#username}
-required="^1 $username /usr/bin/dumb-init"
+required="^($username /usr/bin/dumb-init).*"
 
 # Use bash -c, because otherwise you must specify an absolute path to ps,
 # because $PATH would not be set.
 actual=`docker exec $container bash -c "ps -w -w h -C dumb-init -o pid:1,user:$username_width,args"`
 
-if [ "$actual" =~ "$required" ]; then
+if [[ "$actual" =~ "$required" ]]; then
     printf "required dumb-init not found\n"
     printf "required:\n%s\n" "$required"
     printf "actual:\n%s\n" "$actual"
